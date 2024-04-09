52413dc3b0ba2ccba588b5e00944c7f42154fff6
Yuhan Liu <yuhanl@uchicago.edu>
2024-04-09 06:48:20 +0000

    a faster version

--- src/decoder/setup.py
+++ src/decoder/setup.py
@@ -5,7 +5,18 @@ setup(
     name='torchac_cuda',
     ext_modules=[
         cpp_extension.CUDAExtension('torchac_cuda', 
-                                    [ 'torchac_kernel.cu']),
+                                    ['torchac_kernel.cu']),
+    ],
+    cmdclass={
+        'build_ext': cpp_extension.BuildExtension
+    }
+)
+
+setup(
+    name='torchac_cuda_layer',
+    ext_modules=[
+        cpp_extension.CUDAExtension('torchac_cuda_layer', 
+                                    ['torchac_kernel_layer.cu']),
     ],
     cmdclass={
         'build_ext': cpp_extension.BuildExtension
